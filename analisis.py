4361e0a0fb1cea711135b04d0645e5da52a0c8b3
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-09-20 23:19:23 +0200

    gg

--- analisis.py
+++ analisis.py
@@ -2,4 +2,5 @@ import pandas as pd
 x=pd.read_csv('stocks.csv')
 list=[x['Addoha'],x['AFMA'],x['Afric Indus']]
 y=pd.DataFrame(list)
-print(y.transpose())
+dataset_bueno=y.transpose()
+print(dataset_bueno)
