27950631be13ef41bb46be18a0d8f3df8a7830ef
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-09-20 23:20:25 +0200

    gg

--- analisis.py
+++ analisis.py
@@ -3,4 +3,5 @@ x=pd.read_csv('stocks.csv')
 list=[x['Addoha'],x['AFMA'],x['Afric Indus']]
 y=pd.DataFrame(list)
 dataset_bueno=y.transpose()
+dataset_bueno=dataset_bueno.dropna()
 print(dataset_bueno)
