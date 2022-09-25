ddb8394cfc61681c6e5b8ce466cc2a40c95acb26
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-09-26 01:33:01 +0200

    gg

--- analisis.py
+++ analisis.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 x=pd.read_csv('stocks.csv')
-list=[x['Addoha'],x['AFMA'],x['Afric Indus']]
+list=[x['date'],x['Addoha'],x['AFMA'],x['Afric Indus']]
 y=pd.DataFrame(list)
 dataset_bueno=y.transpose()
 dataset_bueno=dataset_bueno.dropna()
 print(dataset_bueno)
+for column in dataset_bueno.columns:
+    if column!='date':
+        plt.plot(dataset_bueno[column])
+plt.show()
+plt.title('Estados')
+
+
