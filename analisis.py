1bdd0efe1f76982c3d71e4cb9b4c9183a5c03668
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-01 20:07:07 +0200

    gg

--- analisis.py
+++ analisis.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import numpy as np
 
 x=pd.read_csv('stocks.csv')
 list=[x['date'],x['Addoha'],x['AFMA'],x['Afric Indus']]
 y=pd.DataFrame(list)
 dataset_bueno=y.transpose()
 dataset_bueno=dataset_bueno.dropna()
+dataset_bueno=dataset_bueno.drop(['date'],axis=1)
 print(dataset_bueno)
 for column in dataset_bueno.columns:
-    if column!='date':
-        plt.plot(dataset_bueno[column])
+
+    plt.plot(dataset_bueno[column])
 plt.show()
 plt.title('Estados')
+
+
 
 
